e1153881e8e37fd2f224c5343beeb3e0e88dd269
Jesús Casañas <jesusenrique9773@gmail.com>
2022-08-09 20:41:30 -0300

    mostrar titulos de categorías

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,27 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import getGifs from "../helpers/getGifs";
 
 const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, [category]);
 
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola Mundo</p>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
