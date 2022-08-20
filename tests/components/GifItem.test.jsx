442cd84715325d68da678b2a0c78b67beef31565
Jesús Casañas <jesusenrique9773@gmail.com>
2022-08-20 18:24:16 -0300

    test GifItem component

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,13 +1,27 @@
-const { render } = require("@testing-library/react");
+const { render, screen } = require("@testing-library/react");
 import GifItem from "../../src/components/GifItem";
 
 describe("Pruebas en <GifItem />", () => {
   const title = "Leones";
-  const url = "http://leones.com";
+  const url = "http://leones.com/";
   const id = "12345";
 
   test("Debe hacer hacer match con el snapshot", () => {
     const { container } = render(<GifItem title={title} url={url} id={id} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("Debe mostrar la imagen con el URL y el ALT indicado", () => {
+    render(<GifItem title={title} url={url} id={id} />);
+
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("Debe mostrar el título en el componente", () => {
+    render(<GifItem title={title} url={url} id={id} />);
+
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
